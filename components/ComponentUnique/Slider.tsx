2a055fa32ac5e461d86f1176210a962464eeb7ee
undarez <fortuna77320@gmail.com>
2024-01-06 15:16:36 +0100

    modif modal ajout des icons

--- components/ComponentUnique/Slider.tsx
+++ components/ComponentUnique/Slider.tsx
@@ -1,78 +1,124 @@
-"use client"
+'use client'
+import React, { useState } from 'react'
+import Image from 'next/image'
+import {
+   Carousel,
+   CarouselContent,
+   CarouselItem,
+   CarouselNext,
+   CarouselPrevious,
+} from '../ui/carousel'
+import { Card, CardContent } from '../ui/card'
+import { ChevronLeft, ChevronRight, X } from 'lucide-react'
 
-import React, { useState } from 'react';
-import Image from 'next/image';
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '../ui/carousel';
-import { Card, CardContent } from '../ui/card';
-
-const imageTattous = ['546.jpg', 'acceuil2546.jpg', 'debz546.jpg', 'entrée546.jpg', 'fleur546.jpg', 'oiseau546.jpg', 'tattooMangaColor.jpg'];
+const imageTattous = [
+   '546.jpg',
+   'acceuil2546.jpg',
+   'debz546.jpg',
+   'entrée546.jpg',
+   'fleur546.jpg',
+   'oiseau546.jpg',
+   'tattooMangaColor.jpg',
+]
 
 const Slider = () => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [selectedImage, setSelectedImage] = useState('');
-  
-    const openModal = (image: string) => {
-      setSelectedImage(image);
-      setIsModalOpen(true);
-    };
-  
-    const closeModal = () => {
-      setIsModalOpen(false);
-    };
-  
-    // Ajoutez un gestionnaire de clic pour fermer la modale lorsqu'on clique à l'extérieur de l'image
-    const handleModalClick = (event: React.MouseEvent<HTMLDivElement>) => {
-      // Si l'élément cliqué est la modale elle-même (pas son contenu interne),
-      // alors fermez la modale
-      if (event.target === event.currentTarget) {
-        closeModal();
-      }
-    };
-  
-    return (
+   const [isModalOpen, setIsModalOpen] = useState(false)
+   const [selectedImageIndex, setSelectedImageIndex] = useState(0)
+
+   const openModal = (index: number) => {
+      setSelectedImageIndex(index)
+      setIsModalOpen(true)
+   }
+
+   const closeModal = () => {
+      setIsModalOpen(false)
+   }
+
+   const handleNextImage = (event: React.MouseEvent<HTMLButtonElement>) => {
+      event.stopPropagation()
+      setSelectedImageIndex(
+         (prevIndex) => (prevIndex + 1) % imageTattous.length
+      )
+   }
+
+   const handlePreviousImage = (event: React.MouseEvent<HTMLButtonElement>) => {
+      event.stopPropagation()
+      setSelectedImageIndex(
+         (prevIndex) =>
+            (prevIndex - 1 + imageTattous.length) % imageTattous.length
+      )
+   }
+
+   return (
       <div>
-        {/* Votre carousel existant */}
-        <Carousel className="w-full max-w-sm">
-          <CarouselContent>
-            {imageTattous.map((imageTattou, index) => (
-              <CarouselItem key={index} className="pl-1 md:basis-1/2 lg:basis-1/3">
-                <div className="p-1" onClick={() => openModal(imageTattou)}>
-                  <Card>
-                    <CardContent className="flex aspect-square items-center justify-center p-6">
-                      <Image
-                        className="object-cover rounded-lg"
-                        src={`/assets/${imageTattou}`}
-                        alt={`Tattoo image ${index + 1}`}
-                        width={200}
-                        height={200}
-                      />
-                    </CardContent>
-                  </Card>
-                </div>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
-  
-        {/* Modale */}
-        {isModalOpen && (
-          <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center" onClick={handleModalClick}>
-            <div className="modal-content">
-              <Image
-                className="object-cover h-auto w-full bg-background "
-                src={`/assets/${selectedImage}`}
-                alt="Full-size Image"
-                width={800}
-                height={800}
-              />
-              <button onClick={closeModal}>Fermer la modale</button>
+         {/* Votre carousel existant */}
+         <Carousel className="w-full max-w-sm">
+            <CarouselContent>
+               {imageTattous.map((imageTattou, index) => (
+                  <CarouselItem
+                     key={index}
+                     className="pl-1 md:basis-1/2 lg:basis-1/3"
+                  >
+                     <div className="p-1" onClick={() => openModal(index)}>
+                        <Card>
+                           <CardContent className="flex aspect-square items-center justify-center p-6">
+                              <Image
+                                 className="object-cover rounded-lg cursor-pointer"
+                                 src={`/assets/${imageTattou}`}
+                                 alt={`Tattoo image ${index + 1}`}
+                                 width={200}
+                                 height={200}
+                              />
+                           </CardContent>
+                        </Card>
+                     </div>
+                  </CarouselItem>
+               ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+         </Carousel>
+
+         {/* Modale */}
+         {isModalOpen && (
+            <div
+               className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center"
+               onClick={closeModal}
+            >
+               <div
+                  className="modal-content relative flex justify-center items-center"
+                  onClick={(event) => event.stopPropagation()}
+               >
+                  <button
+                     onClick={handlePreviousImage}
+                     className="absolute left-4 top-1/2 transform -translate-y-1/2"
+                  >
+                     <ChevronLeft className="text-green-300 w-8 h-auto bg-white rounded-md " />
+                  </button>
+                  <button
+                     onClick={handleNextImage}
+                     className="absolute right-4 top-1/2 transform -translate-y-1/2"
+                  >
+                     <ChevronRight className="text-green-300 w-8 h-auto bg-white rounded-md " />
+                  </button>
+                  <button
+                     onClick={closeModal}
+                     className="absolute top-4 right-4"
+                  >
+                     <X className="text-red-500 w-6 h-auto bg-white rounded-md" />
+                  </button>
+                  <Image
+                     className="object-cover h-1/2 w-1/2 bg-background rounded-md"
+                     src={`/assets/${imageTattous[selectedImageIndex]}`}
+                     alt="Full-size Image"
+                     width={800}
+                     height={800}
+                  />
+               </div>
             </div>
-          </div>
-        )}
+         )}
       </div>
-    );
-  };
-  
-  export default Slider;
+   )
+}
+
+export default Slider
--- src/Header.tsx
+++ src/Header.tsx
@@ -4,7 +4,10 @@ import Image from 'next/image'
 import UserProfil from './themes/auth/UserProfile'
 import LoginButton from './themes/auth/LogginButton'
 import { ThemeToggle } from './themes/ThemeToggle'
-import { Button } from '@/components/ui/button'
+import { Button, buttonVariants } from '@/components/ui/button'
+import Link from 'next/link'
+import { Home } from 'lucide-react'
+import clsx from 'clsx'
 
 const Header = async () => {
 
@@ -12,6 +15,9 @@ const Header = async () => {
   return (
     <header className="border-b border-b-accent top-0 z-20 py-12 bg-background w-full">
       <div className="container flex items-center justify-center p-5 max-w-lg m-auto gap-1">
+        <Link href="/" className={clsx(buttonVariants({variant:'ghost'}))}><Home/>
+        
+        </Link>
     
       <Image
       className='w-22 flex items-start  m-auto rounded-full shadow-xl shadow-black'
